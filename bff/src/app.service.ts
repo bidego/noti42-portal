c691e0c5701bf38cb5350f8ca163b5a74b2552df
GONZALO TOMAS BIDE <gonzalo.bide@gmail.com>
2025-07-23 15:29:57 -0300

    Mejoras readiness

--- bff/src/app.service.ts
+++ bff/src/app.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+const { join } = require('path');
+const { existsSync } = require('fs');
+
 @Injectable()
 export class AppService {
   getAlive(): string {
     return 'Alive!';
   }
   isReady(): string {
+    // Chequeamos si hay archivos en la carpeta public
+    const publicFolder = join(__dirname, '..', 'public');
+
+    if (!existsSync(publicFolder)) {
+      return 'Not ready! Missing build artifacts';
+    }
     return 'Ready!';
   }
 }
