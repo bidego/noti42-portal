a7cf7bfee65f9d84d2a5c0903a0d486bbe8c6eed
GONZALO TOMAS BIDE <gonzalo.bide@gmail.com>
2025-07-25 19:33:44 -0300

    Mejoras api.controller proxy

--- bff/src/api.controller.ts
+++ bff/src/api.controller.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export class ApiController {
   private logger = new Logger('ApiController');
   // get env variable from .env file
-  private readonly NOTI_BO_BASE_URL = process.env.NOTI_BO_BASE_URL || 'http://localhost:9000'; // Assuming noti-bo runs on port 9000
+  private readonly NOTI_BO_BASE_URL = process.env.NOTI_BO_BASE_URL || 'http://local-admin.noti42.com:9000'; // Assuming noti-bo runs on port 9000
 
   @All('*')
   async proxyAll(@Req() req: Request, @Res() res: Response) {
@@ -16,13 +16,15 @@ export class ApiController {
     delete filteredHeaders['connection']; // Remove connection header
     delete filteredHeaders['content-length']; // Remove content-length header
     delete filteredHeaders['transfer-encoding']; // Remove transfer-encoding header
+    delete filteredHeaders['cookie']; // Remove cookie header from PLAY
 
     // Add the desired Host header
-    filteredHeaders['Host'] = process.env.NOTI_BO_HOST_HEADER || 'admin.noti42.com';
+    filteredHeaders['Host'] = process.env.NOTI_BO_HOST_HEADER || 'local-admin.noti42.com';
 
     const requestBody = ['POST', 'PUT', 'PATCH'].includes(req.method) ? req.body : undefined;
 
     try {
+      this.logger.log(`Proxying request to ${url}`);
       const response = await axios({
         method: req.method,
         url: url,
